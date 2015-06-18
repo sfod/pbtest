ac30e8239786927d8c8a3d60fdb9896102b396c2
sfod <octosyllabic@gmail.com>
2015-06-18 15:22:23 +0300

    Implement sample protobuf usage

--- src/main.c
+++ src/main.c
@@ -1,4 +1,15 @@
+#include <stdlib.h>
+#include "proto/task.pb-c.h"
+
 int main()
 {
+    Task task = TASK__INIT;
+    task.id = 1;
+
+    size_t len = task__get_packed_size(&task);
+    uint8_t *buf = malloc(len);
+    task__pack(&task, buf);
+    free(buf);
+
     return 0;
 }
